820e0c43ae916237a911a795092b1d6f65cb6776
Sapnamarsonia <sapnasapriya555@gmail.com>
2024-11-23 16:22:10 +0530

    commit

---
+++ Module 1/javadt19/question6.java
@@ -0,0 +1,14 @@
+//Write a program to find the simple Interest.
+public class question6 {
+	public static void main(String args[])
+	{
+		// declaring variable 
+		double principal = 1000; // principal amount 
+		double rate = 10;     // rate of interest
+		double time = 2;   // duration of interest 
+		double SI = (principal*rate*time)/ 100; // formula used to calculate SI
+	
+		System.out.println("Simple interest : "+SI);
+	}
+
+}
