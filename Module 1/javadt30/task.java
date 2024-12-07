e4d435c5d196e9ed8c261edcfcb09232f2db8d7f
Sapnamarsonia <sapnasapriya555@gmail.com>
2024-12-07 14:58:58 +0530

    commit

---
+++ Module 1/javadt30/task.java
@@ -0,0 +1,19 @@
+package javadt30;
+
+public class task 
+{
+	public static void main(String[] args)
+	{
+		char ch[] = {'I','N','D','I','A'};
+		String ans = "" ;
+		
+		for( int i=0 ; i< ch.length ; i++)
+		{
+			ans+=ch[i];
+			System.out.println(ans);
+		}
+			
+		
+	}
+	
+}
